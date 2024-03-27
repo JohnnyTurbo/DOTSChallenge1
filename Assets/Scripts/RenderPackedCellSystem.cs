80a1cd4b9d601fdd5fb3adb275f60d4c79029b7d
Johnny Thompson <jthompson@tmg.dev>
2024-03-26 23:33:33 -0700

    Fixed bit manipulation issue causing bits to show up twice.

--- Assets/Scripts/RenderPackedCellSystem.cs
+++ Assets/Scripts/RenderPackedCellSystem.cs
@@ -22,7 +22,7 @@ namespace TMG.GameOfLife
             foreach (var (color, dataEntity) in SystemAPI.Query<RefRW<URPMaterialPropertyBaseColor>, PackedDataEntity>())
             {
                 var cellBytes = cellLookup[dataEntity.Entity].ElementAt(dataEntity.IndexInBuffer).Value;
-                var cellBit = (ulong)(1 << dataEntity.IndexInElement);
+                var cellBit = (ulong)1 << dataEntity.IndexInElement;
                 var isAlive = (cellBit & cellBytes) != 0;
                 
                 color.ValueRW.Value = isAlive ? new float4(0, 1, 0, 1) : new float4(0.25f, 0.25f, 0.25f, 1f);
